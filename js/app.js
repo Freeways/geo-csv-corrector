9bf660c26e34f8eda6cdfb81bf3e00782aac61ae
bechir <bechir@accessnow.org>
2016-08-13 04:49:00 +0100

    popups & sample

--- js/app.js
+++ js/app.js
@@ -9,18 +9,27 @@ function initmap(lat, lng, zoom) {
   var osmAttrib = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
   var osm = new L.TileLayer(osmUrl, {minZoom: 0, maxZoom: 20, attribution: osmAttrib});
 
-  // start the map in South-East England
+  // start the map
   map.setView(new L.LatLng(lat, lng), zoom);
   map.addLayer(osm);
 }
-
+stripTags = function(html){
+  return html.replace(/(<([^>]+)>)/ig,"")
+};
 function processCSV(data) {
   data.forEach(function (d) {
     var marker = L.marker([d.lat, d.lon], {title: d.name, draggable: true}).addTo(map);
+    marker.bindPopup('<p contenteditable="true">' + d.content + '</p>');
+    
     marker.on('dragend', function () {
       var link = document.querySelector('#to-csv');
       link.style.display = 'block';
     });
+    marker.on('click', function(){
+      var popup = this.getPopup();
+      popup.setContent('<p contenteditable="true">' + stripTags(popup._wrapper.innerHTML) + '</p>');
+      this.openPopup();
+    })
     d.marker = marker;
   });
   return data;
@@ -28,7 +37,8 @@ function processCSV(data) {
 function export2CSV() {
   var csv = "name,lat,lon\n"
   markers.forEach(function (d) {
-    csv += d.name + "," + d.marker.getLatLng().lat + "," + d.marker.getLatLng().lng + "\n";
+    var content = stripTags(d.marker.getPopup().getContent());
+    csv += d.name + "," + d.marker.getLatLng().lat + "," + d.marker.getLatLng().lng + "," + content +"\n";
   });
   var a = window.document.createElement('a');
   a.href = window.URL.createObjectURL(new Blob([csv], {type: 'text/csv'}));
