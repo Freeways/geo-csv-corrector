b1db0d621c88bffe358184229e80d059b4eb966b
bechir <bechir@accessnow.org>
2016-08-13 05:10:15 +0100

    full

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 var map;
+var link = document.querySelector('#to-csv');
 //http://a.tile.openstreetmap.org/23/4432298/3269305.png
 function initmap(lat, lng, zoom) {
   // set up the map
@@ -14,31 +15,45 @@ function initmap(lat, lng, zoom) {
   map.addLayer(osm);
 }
 stripTags = function(html){
-  return html.replace(/(<([^>]+)>)/ig,"")
+  return html.replace(/(<([^>]+)>)/ig,"");
 };
 function processCSV(data) {
   data.forEach(function (d) {
     var marker = L.marker([d.lat, d.lon], {title: d.name, draggable: true}).addTo(map);
     marker.bindPopup('<p contenteditable="true">' + d.content + '</p>');
-    
     marker.on('dragend', function () {
-      var link = document.querySelector('#to-csv');
       link.style.display = 'block';
     });
+    marker.state = 1;
     marker.on('click', function(){
-      var popup = this.getPopup();
-      popup.setContent('<p contenteditable="true">' + stripTags(popup._wrapper.innerHTML) + '</p>');
-      this.openPopup();
+      if(this.state){
+        this.closePopup();
+      }else{
+        this.openPopup();
+      }
     })
+    marker.on('popupopen', function(){
+      this.state = 0;
+    });
+    marker.on('popupclose', function(){
+      var popup = this.getPopup();
+      oldContent = popup.getContent();
+      newContent = '<p contenteditable="true">' + stripTags(popup._wrapper.innerHTML) + '</p>';
+      popup.setContent(newContent);
+      this.state = 1;
+      if(oldContent !== newContent)
+        link.style.display = 'block';
+    });
     d.marker = marker;
   });
   return data;
 }
+
 function export2CSV() {
-  var csv = "name,lat,lon\n"
+  var csv = "name,lat,lon,content\n"
   markers.forEach(function (d) {
     var content = stripTags(d.marker.getPopup().getContent());
-    csv += d.name + "," + d.marker.getLatLng().lat + "," + d.marker.getLatLng().lng + "," + content +"\n";
+    csv += d.name + "," + d.marker.getLatLng().lat + "," + d.marker.getLatLng().lng + "," + content + "\n";
   });
   var a = window.document.createElement('a');
   a.href = window.URL.createObjectURL(new Blob([csv], {type: 'text/csv'}));
@@ -46,7 +61,6 @@ function export2CSV() {
   document.body.appendChild(a)
   a.click();
   document.body.removeChild(a)
-  var link = document.querySelector('#to-csv');
   link.style.display = 'none';
 }
 initmap(36.826248, 10.174414, 14);
